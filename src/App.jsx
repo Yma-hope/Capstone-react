43306dfe0ab8a47dc423e967fef4c265ab787ec5
yma <ymanjeri2@gmail.com>
2025-05-16 09:57:35 +0300

    Complete React project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Gallery from './Pages/Gallery'
-import Photographers from'./Pages/Photographers'
-import Contacts from './Pages/Contacts'
-import Reviews from './Pages/Reviews'
-import Login from './Pages/Login'
-import Signup from './Pages/Sign up'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
-import ProtectedRoute from './Components/ProtectedRoute'
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('./Pages/Home'));
+const About = lazy(() => import('./Pages/About'));
+const Gallery = lazy(() => import('./Pages/Gallery'));
+const Photographers = lazy(() => import('./Pages/Photographers'));
+const Contacts = lazy(() => import('./Pages/Contacts'));
+const Reviews = lazy(() => import('./Pages/Reviews'));
+const Login = lazy(() => import('./Pages/Login'));
+const Signup = lazy(() => import('./Pages/Sign up'));
+
+//import Home from './Pages/Home'
+//import About from './Pages/About'
+//import Gallery from './Pages/Gallery'
+//import Photographers from'./Pages/Photographers'
+//import Contacts from './Pages/Contacts'
+//import Reviews from './Pages/Reviews'
+//import Login from './Pages/Login'
+//import Signup from './Pages/Sign up'
+//import Navbar from './Components/Navbar'
+//import Footer from './Components/Footer'
+//import ProtectedRoute from './Components/ProtectedRoute'
 
 
 function App() {
@@ -18,6 +29,7 @@ function App() {
   return (
     <>
         <Navbar />
+        <Suspense fallback = {<div>Loading...</div>}>
         <Routes>
           <Route path='/home' element={<Home />} />
           <Route path='/contacts' element={<Contacts />} />
@@ -58,6 +70,7 @@ function App() {
             }
           />
         </Routes>
+        </Suspense>
         <Footer />
         </>
   );
